958852e2abb848da4557a3e74000e01016a9a69e
Hafidh RK <hfrk.hafidh@gmail.com>
2022-09-17 17:22:30 +0900

    Change database format.

--- backend/lookup-table.js
+++ backend/lookup-table.js
@@ -32,10 +32,10 @@ const SensorName = {
 };
 
 const SQLTables = {
-    R1: "(timestamp DATETIME, Dn VARCHAR(16), Dm VARCHAR(16), Dx VARCHAR(16), Sn VARCHAR(16), Sm VARCHAR(16), Sx VARCHAR(16))",
-    R2: "(timestamp DATETIME, Ta VARCHAR(16), Ua VARCHAR(16), Pa VARCHAR(16))",
-    R3: "(timestamp DATETIME, Rc VARCHAR(16), Rd VARCHAR(16), Ri VARCHAR(16), Rp VARCHAR(16))",
-    R5: "(timestamp DATETIME, Th VARCHAR(16), Vh VARCHAR(16), Vs VARCHAR(16), Vr VARCHAR(16), Id VARCHAR(16))"
+    R1: "(timestamp DATETIME, Dn INT(4), Dm INT(4), Dx INT(4), Sn FLOAT(4), Sm FLOAT(4), Sx FLOAT(4))",
+    R2: "(timestamp DATETIME, Ta FLOAT(4), Ua FLOAT(4), Pa FLOAT(4))",
+    R3: "(timestamp DATETIME, Rc FLOAT(4), Rd INT(4), Ri FLOAT(4), Rp FLOAT(4))",
+    R5: "(timestamp DATETIME, Th FLOAT(4), Vh VARCHAR(16), Vs FLOAT(4), Vr FLOAT(4), Id VARCHAR(16))"
 };
 
 const SQLFields = {
