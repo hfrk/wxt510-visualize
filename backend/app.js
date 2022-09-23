70f191935d8e7ebd38baa8c6d0f14456616ed7d0
Hafidh RK <hfrk.hafidh@gmail.com>
2022-09-23 16:05:13 +0900

    Separate express app and socket listener.

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,16 @@
+// Backend
 require('dotenv').config();
 
-const DataParser = require("./parser.js");
-const DBLogger = require("./dblogger.js");
-const Windrose = require("./windrose.js");
-
-const host = process.env.SENSOR_HOST;
-const port = parseInt(process.env.SENSOR_PORT);
-
-// Backend
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
+
+const DataListener = require("./data-listener.js");
+const Windrose = require("./windrose.js");
+const Rain = require("./rain.js");
 
 const app = express();
-const cors = require('cors');
-let sensorData = {};
+const serverPort = 4000;
 
 app.use(cors());
 
@@ -21,48 +18,30 @@ app.use('/windrose', async (req, res) => {
     let data = await Windrose.getData();
     res.status(200).send(JSON.stringify(data, null, 2))
 });
+app.use('/rain', async (req, res) => {
+    let data = await Rain.getData();
+    res.status(200).send(JSON.stringify(data, null, 2))
+});
 app.use('/data', async (req, res) => {
-  res.status(200).send(JSON.stringify(sensorData, null, 2));
+    let data = DataListener.getData();
+    res.status(200).send(JSON.stringify(data, null, 2));
 });
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.use((err, req, res, next) => {
-  console.log(err.message);
-  console.log(JSON.stringify(err.stack));
+    console.log(err.message);
+    console.log(JSON.stringify(err.stack));
 });
 
-app.listen(4000, () => {
-  console.log(`Server started on port ${port}`);
-});
-
-// Data Listener
-const net = require('net');
-const client = new net.Socket();
-
-client.connect(port, host, function() {
-    console.log('Connected to sensor');
-});
-
-let buffer = "";
-client.on('data', function(data) {
-    //console.log(data, data.length);
-    buffer += String(data);
-    if (buffer.includes("\r\n")) {
-        let chunks = buffer.split("\r\n");
-        buffer = chunks.pop();
-        chunks.forEach((chunk) => {
-            let parsedData = DataParser.parse(chunk);
-            DBLogger.log(parsedData);
-            //console.log(parsedData);
-            sensorData/*[parsedData.type]*/ = parsedData;
-        });
-        //client.destroy(); // kill client after server's response
-    }
-});
-
-client.on('close', function() {
-    console.log('Connection closed');
+app.listen(serverPort, () => {
+    console.log(`Server started on port ${serverPort}`);
+})
+.on("error", (err) => {
+    if (err.code === 'EADDRINUSE')
+        throw new Error(`Port ${serverPort} is busy`);
+    else
+        throw err;
 });
---
+++ backend/data-listener.js
@@ -0,0 +1,67 @@
+// Data Listener
+require('dotenv').config();
+
+const net = require('net');
+const DBLogger = require("./dblogger.js");
+const DataParser = require("./parser.js");
+
+const host = process.env.SENSOR_HOST;
+const port = parseInt(process.env.SENSOR_PORT);
+
+const client = new net.Socket();
+const reconnectInterval = 10000; // 10 sec
+let intervalConnect = false;
+
+let sensorData = {};
+let buffer = "";
+
+const connect = () => {
+    client.connect(port, host, function() {
+        console.log('Connected to sensor');
+    });
+};
+
+const launchIntervalConnect = () => {
+    if(intervalConnect) return;
+    intervalConnect = setInterval(connect, reconnectInterval);
+};
+
+const clearIntervalConnect = () => {
+    if(!intervalConnect) return;
+    clearInterval(intervalConnect);
+    intervalConnect = false;
+};
+
+client.on('connect', () => {
+    console.log("Connected");
+    clearIntervalConnect();
+});
+
+client.on('data', function(data) {
+    //console.log(data, data.length);
+    buffer += String(data);
+    if (buffer.includes("\r\n")) {
+        let chunks = buffer.split("\r\n");
+        buffer = chunks.pop();
+        chunks.forEach((chunk) => {
+            let parsedData = DataParser.parse(chunk);
+            DBLogger.log(parsedData);
+            //console.log(parsedData);
+            sensorData/*[parsedData.type]*/ = parsedData;
+        });
+        //client.destroy(); // kill client after server's response
+    }
+});
+
+client.on('error', (err) => {
+    console.log(err);
+    launchIntervalConnect();
+});
+
+client.on('close', launchIntervalConnect);
+client.on('end', launchIntervalConnect);
+connect();
+
+module.exports.getData = () => {
+    return sensorData;
+};
